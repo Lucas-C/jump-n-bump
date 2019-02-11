66819900e3d64fcb261d7972a38eb917f2097507
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-11 07:03:32 +0000

    Deploy Lucas-C/jump-n-bump to github.com/Lucas-C/jump-n-bump.git:gh-pages

---
+++ README.md
@@ -0,0 +1,28 @@
+[![](https://travis-ci.org/Lucas-C/jump-n-bump.svg?branch=master)](https://travis-ci.org/Lucas-C/jump-n-bump)
+
+# jump-n-bump
+HTML5 port by [GrahamTheCoder](https://github.com/GrahamTheCoder) of the [open source](https://github.com/fschulze/jumpnbump) [DOS classic](https://icculus.org/jumpnbump/) by [Brainchild Design](http://www.bitbliss.com)
+
+Keyboard map
+
+M: toggle sound
+
+1-4: toggle player 1-4
+
+Alt + 1-4: toggle ai for player 1-4
+
+Arrows: control player 1
+
+WAD: control player 2
+
+JIL: control player 3
+
+NumPad: control player 4
+
+## Development
+
+1. First run: `npm install`
+1. To rebuild `npm run build`
+1. To rebuild whenever files change `npm run watch`
+1. Open `game/index.html` in a browser to play
+
---
+++ src/interaction/renderer.js
@@ -0,0 +1,69 @@
+import { player } from "../game/game";
+import { env } from "../interaction/game_session";
+import { rabbit_gobs } from "../asset_data/rabbit_gobs";
+
+export function Renderer(canvas, img, level) {
+    "use strict";
+    var main = { num_pobs: 0, pobs: [] };
+    var leftovers = { num_pobs: 0, pobs: [] };
+    var ctx = canvas.getContext('2d');
+
+    var MAX = {
+        POBS: 200,
+        FLIES: 20,
+        LEFTOVERS: 50,
+    };
+
+    this.add_leftovers = function(x, y, image, gob) {
+        leftovers.pobs[leftovers.num_pobs] = { x: x, y: y, gob: gob, image: image };
+        leftovers.num_pobs++;
+    }
+
+    this.add_pob = function(x, y, image, gob) {
+        if (main.num_pobs >= MAX.POBS) {
+            return;
+        }
+        main.pobs[main.num_pobs] = { x: x, y: y, gob: gob, image: image };
+        main.num_pobs++;
+    }
+
+    function put_pob(x, y, gob, img) {
+        var sx, sy, sw, sh, hs_x, hs_y;
+        sx = gob.x;
+        sy = gob.y;
+        sw = gob.width;
+        sh = gob.height;
+        hs_x = gob.hotspot_x;
+        hs_y = gob.hotspot_y;
+        ctx.drawImage(img, sx, sy, sw, sh, x - hs_x, y - hs_y, sw, sh);
+    }
+
+    function draw_pobs() {
+        for (var c1 = main.num_pobs - 1; c1 >= 0; c1--) {
+            var pob = main.pobs[c1];
+            put_pob(pob.x, pob.y, pob.gob, pob.image);
+        }
+    }
+
+    function draw_leftovers() {
+        for (var c1 = 0; c1 != leftovers.num_pobs; ++c1) {
+            var pob = leftovers.pobs[c1];
+            put_pob(pob.x, pob.y, pob.gob, pob.image);
+        }
+    }
+
+    this.draw = function () {
+        ctx.drawImage(level.image, 0, 0);
+
+        for (var i = 0; i < env.JNB_MAX_PLAYERS; i++) {
+            if (player[i].enabled) {
+                this.add_pob(player[i].x.pos >> 16, player[i].y.pos >> 16, img.rabbits, rabbit_gobs[player[i].get_image() + i * 18]);
+            }
+        }
+        draw_leftovers();
+        draw_pobs();
+
+        ctx.drawImage(level.mask, 0, 0);
+        main.num_pobs = 0;
+    }
+};
